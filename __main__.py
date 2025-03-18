186a15f0fd90c847e9c855d759e92230094e254b
MitchellGerdisch <mitch@pulumi.com>
2025-03-18 08:51:55 -0500

    initial commit

---
+++ __main__.py
@@ -0,0 +1,7 @@
+from pulumi.provider.experimental import Metadata, component_provider_host
+
+if __name__ == "__main__":
+    # Call the component provider host. This will discover any ComponentResource
+    # subclasses in this package, infer their schema and host a provider that
+    # allows constructing these components from a Pulumi program.
+    component_provider_host(Metadata("aws-abstracted", "0.1.0"))
